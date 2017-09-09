5ab1397eb11cad9e40ad7f2d0468610ca1b57502
Your Name <vanhoang2604@gmail.com>
2017-09-09 22:03:58 +0700

    hoang

--- app/src/main/java/teampass/gympass/MainActivity.java
+++ app/src/main/java/teampass/gympass/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nutrition);
+        setContentView(R.layout.activity_nutrition_02);
     }
 }
