16fe47ae31ff80b457ac03359bc6b04907045137
anhboy2002 <anhboy2002@gmail.com>
2017-09-15 13:08:59 +0700

    hungpro

--- app/src/main/java/teampass/gympass/MainActivity.java
+++ app/src/main/java/teampass/gympass/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nutrition_02);
+        setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/teampass/gympass/model/menu.java
@@ -0,0 +1,9 @@
+package teampass.gympass.model;
+
+/**
+ * Created by DELL on 9/15/2017.
+ */
+
+public class menu {
+    private
+}
