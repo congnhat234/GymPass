19b32825a9fa313280340a09fecb746dfcfa5218
Cong Nhat <congnhat.234@gmail.com>
2017-09-17 00:07:53 +0700

    nhat

--- app/src/main/java/teampass/gympass/MainActivity.java
+++ app/src/main/java/teampass/gympass/MainActivity.java
@@ -1,8 +1,14 @@
 package teampass.gympass;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -30,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-
 
     }
 }
