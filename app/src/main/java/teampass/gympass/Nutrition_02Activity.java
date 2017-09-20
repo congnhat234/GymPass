4c9b520765199c16e99beac6164472098dc3c0dd
Your Name <vanhoang2604@gmail.com>
2017-09-20 14:53:14 +0700

    hoang

--- app/src/main/java/teampass/gympass/Nutrition_02Activity.java
+++ app/src/main/java/teampass/gympass/Nutrition_02Activity.java
@@ -2,12 +2,42 @@ package teampass.gympass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import teampass.gympass.adapter.ListMenu;
+import teampass.gympass.model.Menun;
 
 public class Nutrition_02Activity extends AppCompatActivity {
+    private ListView lvGiamcan;
+    private ArrayList<Menun> mMenun = new ArrayList<Menun>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutrition_02);
+        doCreateFakeData();
+
+        lvGiamcan = (ListView) findViewById(R.id.lvGiamcan);
+        ListMenu adapter = new ListMenu(Nutrition_02Activity.this, R.layout.item_menu,mMenun);
+        lvGiamcan.setAdapter(adapter);
+    }
+    private void doCreateFakeData() {
+        Menun m1 = new Menun(R.drawable.giamcant2, "Thực đơn thứ 2");
+        Menun m2 = new Menun(R.drawable.giamcant3, "Thực đơn thứ 3");
+        Menun m3 = new Menun(R.drawable.giamcant4, "Thực đơn thứ 4");
+        Menun m4 = new Menun(R.drawable.giamcant5, "Thực đơn thứ 5");
+        Menun m5 = new Menun(R.drawable.giamcant6, "Thực đơn thứ 6");
+        Menun m6 = new Menun(R.drawable.giamcant7, "Thực đơn thứ 7");
+        Menun m7 = new Menun(R.drawable.giamcant8, "Thực đơn chủ nhật");
+
+        mMenun.add(m1);
+        mMenun.add(m2);
+        mMenun.add(m3);
+        mMenun.add(m4);
+        mMenun.add(m5);
+        mMenun.add(m6);
+        mMenun.add(m7);
     }
 }
--- app/src/main/java/teampass/gympass/Nutrition_04Activity.java
+++ app/src/main/java/teampass/gympass/Nutrition_04Activity.java
@@ -1,13 +1,45 @@
 package teampass.gympass;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import teampass.gympass.adapter.ListMenu;
+import teampass.gympass.model.Menun;
 
 public class Nutrition_04Activity extends AppCompatActivity {
+    private ListView lvThucpham;
+    private ArrayList<Menun> mMenun = new ArrayList<Menun>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutrition_04);
+        doCreateFakeData();
+
+        lvThucpham = (ListView) findViewById(R.id.lvThucpham);
+        ListMenu adapter = new ListMenu(Nutrition_04Activity.this,R.layout.item_menu,mMenun);
+        lvThucpham.setAdapter(adapter);
+    }
+    private void doCreateFakeData() {
+        Menun m1 = new Menun(R.drawable.thucpham1, "Thực phẩm----");
+        Menun m2 = new Menun(R.drawable.thucpham2, "Thực phẩm----");
+        Menun m3 = new Menun(R.drawable.thucpham3, "Thực phẩm----");
+        Menun m4 = new Menun(R.drawable.thucpham4, "Thực phẩm----");
+        Menun m5 = new Menun(R.drawable.thucpham5, "Thực phẩm----");
+        Menun m6 = new Menun(R.drawable.thucpham6, "Thực phẩm---- ");
+        Menun m7 = new Menun(R.drawable.thucpham7, "Thực phẩm----");
+        Menun m8 = new Menun(R.drawable.thucpham8, "Thực phẩm----");
+
+        mMenun.add(m1);
+        mMenun.add(m2);
+        mMenun.add(m3);
+        mMenun.add(m4);
+        mMenun.add(m5);
+        mMenun.add(m6);
+        mMenun.add(m7);
+        mMenun.add(m8);
     }
 }
