2b1e1ee24a5abc3c4b4390863b978519834c3671
Your Name <vanhoang2604@gmail.com>
2017-09-20 16:47:38 +0700

    hoang

--- app/src/main/java/teampass/gympass/ExerciseActivity.java
+++ app/src/main/java/teampass/gympass/ExerciseActivity.java
@@ -1,17 +1,13 @@
 package teampass.gympass;
 
-import android.app.ActionBar;
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class ExerciseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
--- app/src/main/java/teampass/gympass/MainActivity.java
+++ app/src/main/java/teampass/gympass/MainActivity.java
@@ -1,15 +1,9 @@
 package teampass.gympass;
 
 import android.content.Intent;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,25 +11,39 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imgworkout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sliding_menu);
-        imgcustom = (ImageView) findViewById(R.id.img_custom);
-        imgworkout = (ImageView) findViewById(R.id.img_workout);
-        imgcustom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, NutritionActivity.class);
-                startActivity(intent);
-            }
-        });
-        imgworkout.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, WorkoutsActivity.class);
-                startActivity(intent);
-            }
+        setContentView(R.layout.activity_main);
 
-        });
-
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(MainActivity.this, ExerciseActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
     }
 }
+
+//        Intent intent = new Intent(this,ExerciseActivity.class);
+//        startActivity(intent);
+//        finish();
+//        imgcustom = (ImageView) findViewById(R.id.img_custom);
+//        imgworkout = (ImageView) findViewById(R.id.img_workout);
+//        imgcustom.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, NutritionActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//        imgworkout.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, WorkoutsActivity.class);
+//                startActivity(intent);
+//            }
+//
+//        });
+
+
+
